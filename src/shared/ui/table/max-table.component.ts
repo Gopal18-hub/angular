76696b3a35ee657a3fb88f0c752e350233149ea9
Veeru Reddy <mandli.reddy@gavstech.com>
2022-06-10 14:35:10 +0530

    table component fixes

--- src/shared/ui/table/max-table.component.ts
+++ src/shared/ui/table/max-table.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatSort, Sort} from '@angular/material/sort';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 @Component({
   selector: 'maxhealth-table',
   templateUrl: './max-table.component.html',
   styleUrls: ['./max-table.component.scss']
 })
-export class MaxTableComponent implements OnInit {
+export class MaxTableComponent implements OnInit, AfterViewInit  {
 
   @Input() config: any;
 
@@ -19,7 +21,9 @@ export class MaxTableComponent implements OnInit {
   displayedColumns: string[] = [];
   displayColumnsInfo: any  = [];
 
-  constructor() { }
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) { }
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<any>(this.data);
@@ -28,7 +32,24 @@ export class MaxTableComponent implements OnInit {
     if(this.config.selectBox) {
       this.displayedColumns.unshift('select');
     }
-    console.log(this.displayedColumns);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 
