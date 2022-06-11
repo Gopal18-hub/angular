f60135652f0cf8e1843f7ff2fcefdf9f18cffe09
Manisha <manisha.manimaran@gavstech.com>
2022-06-11 20:40:30 +0530

    Duplicate Reg Merge API, Table, Dialog added

--- src/out_patients/modules/registration/index.ts
+++ src/out_patients/modules/registration/index.ts
@@ -5,10 +5,18 @@ import { OpRegistrationComponent } from './submodules/op-registration/op-registr
 import { FindPatientComponent } from './submodules/find-patient/find-patient.component';
 import { DupRegMergingComponent } from './submodules/dup-reg-merging/dup-reg-merging.component';
 import { RegistrationUnmergingComponent } from './submodules/registration-unmerging/registration-unmerging.component';
-import { RegistrationComponent } from './registration.component';
-
+import { RegistrationComponent } from './registration.component'
 import { MaxHealthTableModule } from '../../../shared/ui/table';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MergeDialogComponent } from './submodules/dup-reg-merging/merge-dialog/merge-dialog.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -17,12 +25,26 @@ import { CommonModule } from '@angular/common';
        FindPatientComponent,
        DupRegMergingComponent,
        RegistrationUnmergingComponent,
-       RegistrationComponent
+       RegistrationComponent,
+       MergeDialogComponent
   ],
   imports: [
     RegistrationRoutingModule,
     MaxHealthTableModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatCheckboxModule
+    
+
+
   ],
   exports: [],
   providers: [],
--- src/out_patients/modules/registration/submodules/dup-reg-merging/dup-reg-merging.component.ts
+++ src/out_patients/modules/registration/submodules/dup-reg-merging/dup-reg-merging.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import { getallpatient } from '../../../../../out_patients/core/models/findpatientmodel';
+import { environment } from '@environments/environment';
+import { HttpService } from '../../../../../shared/services/http.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MaxTableComponent } from '../../../../../shared/ui/table/max-table.component';
+import { RegistrationUnmergingComponent } from '../registration-unmerging/registration-unmerging.component';
+import { MergeDialogComponent } from './merge-dialog/merge-dialog.component';
+
+
 
 @Component({
   selector: 'out-patients-dup-reg-merging',
@@ -6,10 +16,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dup-reg-merging.component.scss']
 })
 export class DupRegMergingComponent implements OnInit {
+  patientList: getallpatient[] = [];
+  results: any;
+  showpatient: boolean = false;
+  name = '';
+  dob = '';
+  email = '';
+  healthId = '';
+  aadhaarId = '';
+  mobile = '';
 
-  constructor() { }
+  @ViewChild("table") tableRows!: MaxTableComponent
 
-  ngOnInit(): void {
+  constructor(private http: HttpService, public matDialog: MatDialog) { }
+
+  config: any = {
+    selectBox: true,
+    displayedColumns: ['maxid', 'ssn', 'date', 'firstName', 'age', 'gender', 'dob', 'place', 'phone', 'category'],
+    columnsInfo: {
+      maxid: {
+        title: 'Max ID',
+        type: 'number'
+      },
+      ssn: {
+        title: 'SSN',
+        type: 'number'
+      },
+      date: {
+        title: 'Regn.Date',
+        type: 'date'
+      },
+      firstName: {
+        title: 'Name',
+        type: 'string'
+      },
+      age: {
+        title: 'Age',
+        type: 'number'
+      },
+      gender: {
+        title: 'Gender',
+        type: 'string'
+      },
+      dob: {
+        title: 'Date of Birth',
+        type: 'date'
+      },
+      place: {
+        title: 'Address',
+        type: 'string'
+      },
+      phone: {
+        title: 'Phone No.',
+        type: 'number'
+      },
+      category: {
+        title: 'Category'
+      }
+    }
   }
 
+  ngOnInit(): void { }
+
+  openDialog() {
+   this.matDialog.open(MergeDialogComponent,{data:{name:"ABC"}})
+  }
+
+  searchPatient() {    
+    this.patientList = [];
+    this.getAllpatientsBySearch().subscribe((resultData) => {     
+      this.results = resultData as getallpatient[];
+      this.showpatient = true;     
+    })  
+
+  } 
+
+
+  getAllpatientsBySearch() {
+    return this.http.get(environment.PatientApiUrl + 'api/patient/getallpatientssearch?MaxId=' + '' + '&SSN=' + '' + '&Name=' + this.name + '&PhoneNumber=' + this.mobile + '&DOB=' + this.dob + '&AadhaarId=' + this.aadhaarId + '&HealthId=' + this.healthId);
+  }
+  patientMerging() {
+    return this.http.get(environment.PatientApiUrl + 'api/patient/patientmerging/' + "activateUserid" + "userid")
+  }
 }
