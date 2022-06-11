2ea547159c0eccd1f1d693c71eda6f89c839e45b
Piyush Bhole <>
2022-06-11 17:11:45 +0000

    Updated index.ts

--- src/out_patients/modules/registration/index.ts
+++ src/out_patients/modules/registration/index.ts
@@ -6,7 +6,6 @@ import { FindPatientComponent } from './submodules/find-patient/find-patient.com
 import { DupRegMergingComponent } from './submodules/dup-reg-merging/dup-reg-merging.component';
 import { RegistrationUnmergingComponent } from './submodules/registration-unmerging/registration-unmerging.component';
 import { RegistrationComponent } from './registration.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MaxHealthTableModule } from '../../../shared/ui/table';
 import { OpRegApprovalComponent } from './submodules/op-reg-approval/op-reg-approval.component';
 import { MatButtonModule } from '@angular/material/button';
