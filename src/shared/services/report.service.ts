47c7c7196b90e8157ee6be9b3d976e06c6dcbb27
105587 <vimala.muthu@gavstech.com>
2022-06-16 20:58:34 +0530

    Added New service for Report

---
+++ src/shared/services/report.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'; 
+import { Router } from '@angular/router';
+
+
+@Injectable({
+    providedIn: "root",
+  })
+
+  export class ReportService {    
+    constructor(private http: HttpClient,private router: Router) { }  
+    baseUrl: string | undefined;
+    
+    getOPRegistrationForm(_iacode: string ) {  
+      this.baseUrl = environment.ReportsApiUrl+'MAXHIS/Opd_Registration/PrintFormReport'       
+    window.open(this.baseUrl + "?"+_iacode, '_blank', 'toolbar=0,location=0,menubar=0'); 
+  }  
+  getOPRegistrationPrintLabel(_iacode: string ) {  
+    this.baseUrl = environment.ReportsApiUrl+'MAXHIS/Opd_Registration/PrintLabel'    
+  window.open(this.baseUrl + "?"+_iacode, '_blank', 'toolbar=0,location=0,menubar=0'); 
+} 
+getOPRegistrationOrganDonorForm(_iacode: string ) {  
+  this.baseUrl = environment.ReportsApiUrl+'MAXHIS/Opd_Registration/PrintOrganDonorForm' 
+  window.open(this.baseUrl + "?"+_iacode, '_blank', 'toolbar=0,location=0,menubar=0'); 
+}
+  }
