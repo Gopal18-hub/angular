621f817fdf8feee0893939466ebadea609d2ecdc
Veeru Reddy <mandli.reddy@gavstech.com>
2022-07-05 01:31:03 +0530

    fix for the already set the default value

--- src/shared/ui/dynamic-forms/dynamic-form-question.component.ts
+++ src/shared/ui/dynamic-forms/dynamic-form-question.component.ts
@@ -230,8 +230,17 @@ export class DynamicFormQuestionComponent
 
     this.subscription = this.trigger.panelClosingActions.subscribe(
       (e) => {
+        console.log(e);
         if ((!e || !e.source) && !this.question.allowSearchInput) {
-          this.form.controls[this.question.key].setValue(null);
+          const selected = this.question.options
+            .map((option: any) => option.value)
+            .find(
+              (option: any) =>
+                option === this.form.controls[this.question.key].value.value
+            );
+          if (selected == null) {
+            this.form.controls[this.question.key].setValue(null);
+          }
         }
       },
       (err) => this._subscribeToClosingActions(),
