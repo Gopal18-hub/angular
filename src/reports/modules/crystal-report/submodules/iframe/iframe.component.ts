b83125814e13b0b3b195db66e920b6d415e67c3f
300605 <abirami.murugadoss@gavstech.com>
2022-07-07 17:37:28 +0530

    Changes in iframecomponent

--- src/reports/modules/crystal-report/submodules/iframe/iframe.component.ts
+++ src/reports/modules/crystal-report/submodules/iframe/iframe.component.ts
@@ -14,7 +14,15 @@ export class IframeComponent implements OnInit {
 
   ngOnInit(): void {
     this.url = this.san.bypassSecurityTrustResourceUrl(
-      CrystalReport.printOrganDonorForm("test")
+      
+     CrystalReport.billingreport("253266",5)
+     // CrystalReport.printOrganDonorForm("BHTN.230538")
+      // CrystalReport.DailyCollectionReport()
+      // CrystalReport.DiabeticPlanModifyreport("08/19/2021","03/31/2023","Quit Alcohol program (PPG)","Tobbaco Cessation Program","Others Plan","18500")
+      // CrystalReport.DiabeticPlanMainReport()
+     // CrystalReport.DiabeticPlanNewreport()
+     // CrystalReport.OnlinePaymentDetailReport()
     );
+    console.log(this.url);
   }
 }
