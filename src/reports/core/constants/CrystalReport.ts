6c99f45b5ee0d415c9ea9f2190c19c96fe71272e
Sabarirajanm_GAVSTec <sabarirajan.mani@gavstech.com>
2022-07-08 10:29:12 +0530

    Conflict removed.

--- src/reports/core/constants/CrystalReport.ts
+++ src/reports/core/constants/CrystalReport.ts
@@ -1,105 +1,47 @@
 import { environment } from "@environments/environment";
 
 export namespace CrystalReport {
-  export const printOrganDonorForm = (maxId: string) => {
-    return `https://MaxHIS-Reports-sit.maxhealthcare.in/PrintOrganDonorForm?${maxId}`;
+  export const test =
+    "https://MaxHIS-Reports-sit.maxhealthcare.in/PrintOrganDonorForm?BHTN.230538";
+
+  export const printOrganDonorForm = (params: any) => {
+    return `https://MaxHIS-Reports-sit.maxhealthcare.in/PrintOrganDonorForm?${params.maxId}`;
   };
-  export const createOPVisitSlip = (VisitNo: string) => {
+
+  export const billingreport = (params: any) => {
     return (
-      environment.ReportsSampleurl + "CreateOPVisitSlip?VisitNo=${VisitNo}"
-    );
-  };
-  export const consumabaleEntryDetails = (
-    MAXID: string,
-    locationID: number,
-    billno: string
-  ) => {
-    return (
-      environment.ReportsSampleurl +
-      "Billing/OTBilling/ConsumabaleEntryDetails?MAXID=${MAXID}&locationID=${locationID}&billno=${billno}"
-    );
-  };
-  export const planLedger = (
-    tempbalance: number,
-    planid: number,
-    MaxID: string,
-    CmbPlan: string
-  ) => {
-    return (
-      environment.ReportsSampleurl +
-      "Marketing/PlanLedger?$tempbalance={tempbalance}&planid=${planid}&MaxID=${MaxID}&CmbPlan=${CmbPlan}"
-    );
-  };
-  export const planSummary = (dtpStartDate: string, dtpEndDate: string) => {
-    return (
-      environment.ReportsSampleurl +
-      "/Marketing/PlanSummary?dtpStartDate=${dtpStartDate}&dtpEndDate=${dtpEndDate}"
-    );
-  };
-  export const doctorSchedule = (
-    dtpStartDate: string,
-    dtpEndDate: string,
-    rd_Doctor: boolean,
-    datetype: number,
-    location: number,
-    DocID: number
-  ) => {
-    return (
-      environment.ReportsSampleurl +
-      "/FrontOfficeReports/DoctorShedule?dtpStartDate=${dtpStartDate}&dtpEndDate=${dtpEndDate}&rd_Doctor=${rd_Doctor}&location=${location}&DocID=${DocID}"
-    );
-  };
-  export const cRPExpiredPatientDetail = (
-    fromdate: string,
-    todate: string,
-    locationid: number,
-    LocationName: string,
-    user: string
-  ) => {
-    return (
-      environment.ReportsSampleurl +
-      "/FrontOfficeReports/CRPExpiredPatientDetail?fromdate=${fromdate}&todate=${todate}&locationid=${locationid}&Locationname=${Locationname}&user=&{user}"
+      environment.ReportsSampleUrl +
+      `/Billing/OpBillingReport?opbillid=${params.opbillid}&locationID=${params.locationID}`
     );
   };
 
-  export const cROPItemPriceModified = (
-    fromdate: string,
-    todate: string,
-    locationid: number,
-    user: string,
-    LocationName: string
-  ) => {
+  export const DispatchReport = (params: any) => {
+    return environment.ReportsSampleUrl + `/Dispatch_Report/DispatchReport`;
+  };
+
+  export const DiabeticPlanMainReport = (params: any) => {
+    return environment.ReportsSampleUrl + `DiabeticPlanMain/DiabeticPlanMain`;
+  };
+  // from_date=08/19/2021&to_date=03/31/2023&name1=Quit%20Alcohol%20program%20(PPG)&department=Tobbaco%20Cessation%20Program&planTypeName=Others%20Plan&planAmount=18500
+  export const DiabeticPlanModifyreport = (params: any) => {
     return (
-      environment.ReportsSampleurl +
-      "MIS/CROPItemPriceModified?fromdate=${fromdate}&todate=${todate}&locationid=${locationid}&user=${user}LocationName=${LocationName}"
+      environment.ReportsSampleUrl +
+      `/DiabeticPlanModify/DiabeticPlanModify?from_date=${params.from_date}&to_date=${params.to_date}&name1=${params.name1}&department=${params.department}&planTypeName=${params.planTypeName}&planAmount=${params.planAmount}`
     );
   };
-  export const openScrollReport = (
-    fromdate: string,
-    todate: string,
-    flag: number,
-    cmbLocation: number,
-    user: string,
-    LocationName: string,
-    openscroll: string
-  ) => {
+
+  export const DiabeticPlanNewreport = (params: any) => {
     return (
-      environment.ReportsSampleurl +
-      "MIS/OpenScrollReport?fromdate=${fromdate}&todate=${todate}&user=${user}&flag=${flag}&cmbLocation=${cmbLocation}&openscroll=${openscroll}"
+      environment.ReportsSampleUrl +
+      `/DiabeticPlanNew/DiabeticPlanNew?from_date=08/19/2021&to_date=03/31/2023&name1=Quit%20Alcohol%20program%20(PPG)&department=Tobbaco%20Cessation%20Program&planTypeName=Others%20Plan&planAmount=18500`
     );
   };
-  export const ServiceTaxReportData = (
-    fromdate: string,
-    todate: string,
-    cmbLocation: number,
-    inttype: number,
-    rbIP: boolean,
-    LocationName: string,
-    user: string
-  ) => {
-    return (
-      environment.ReportsSampleurl +
-      "MIS/ServiceTaxReportData?fromdate=${fromdate}&todat=e${todate}&cmbLocation=${cmbLocation}&inttype=${inttype}&LocationName=${LocationName}&rbIP=${rbIP}&user=${user}"
-    );
+
+  export const DailyCollectionReport = (params: any) => {
+    return environment.ReportsSampleUrl + `/MIS/DailyCollectionReport`;
+  };
+
+  export const OnlinePaymentDetailReport = (params: any) => {
+    return environment.ReportsSampleUrl + `/MIS/OnlinePaymentDetailReport`;
   };
 }
