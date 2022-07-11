8b1ffc9432f81ed50859955c9eb867eab0b305a6
Piyush Bhole <>
2022-07-11 07:12:49 +0000

    Updated payment-methods.component.ts

--- src/out_patients/core/UI/billing/submodules/payment-methods/payment-methods.component.ts
+++ src/out_patients/core/UI/billing/submodules/payment-methods/payment-methods.component.ts
@@ -1,32 +1,28 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { BillingForm } from "@core/constants/BillingForm";
-import { MatTabChangeEvent, MatTabGroup } from "@angular/material/tabs";
-import { QuestionControlService } from "../../../../../../shared/ui/dynamic-forms/service/question-control.service";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { BillingForm } from '@core/constants/BillingForm';
+import { QuestionControlService } from '../../../../../../shared/ui/dynamic-forms/service/question-control.service';
 @Component({
-  selector: "payment-methods",
-  templateUrl: "./payment-methods.component.html",
-  styleUrls: ["./payment-methods.component.scss"],
+  selector: 'payment-methods',
+  templateUrl: './payment-methods.component.html',
+  styleUrls: ['./payment-methods.component.scss']
 })
 export class PaymentMethodsComponent implements OnInit {
-  @Input() fromrefund!: boolean;
-  @Input() fromdeposit!: boolean;
-  @Output() paymentform: EventEmitter<FormGroup> = new EventEmitter();
-  refundFormData = BillingForm.refundFormData;
+  @Input() config: any;
+  @Output() paymentform:EventEmitter<FormGroup> = new EventEmitter();
+
+  refundFormData =  BillingForm.refundFormData;
   refundform!: FormGroup;
   questions: any;
   today: any;
-  forrefundpage: boolean = true;
-  fordepositpage: boolean = true;
-  constructor(private formService: QuestionControlService) {}
+  constructor( private formService: QuestionControlService) { }
 
   ngOnInit(): void {
     let formResult: any = this.formService.createForm(
       this.refundFormData.properties,
       {}
     );
-    this.forrefundpage = this.fromrefund;
-    this.fordepositpage = this.fromrefund;
     this.refundform = formResult.form;
     this.paymentform.emit(this.refundform);
     this.questions = formResult.questions;
@@ -34,9 +30,11 @@ export class PaymentMethodsComponent implements OnInit {
     this.refundform.controls["chequeissuedate"].setValue(this.today);
     this.refundform.controls["demandissuedate"].setValue(this.today);
   }
-  onTabChanged(event: MatTabChangeEvent) {
-    if (event.index == 1) {
-      this.refundform.controls["amount"].setValue("");
-    }
+
+  tabChanged(event:MatTabChangeEvent){
+    console.log(event);
+    this.refundform.reset();
+    this.refundform.controls["chequeissuedate"].setValue(this.today);
+    this.refundform.controls["demandissuedate"].setValue(this.today);
   }
 }
