ef38f79d5c1c53ede395738af3fbbbe517bb3c94
Piyush Bhole <>
2022-07-11 07:42:10 +0000

    Updated deposit.component.ts

--- src/out_patients/modules/billing/submodules/deposit/deposit.component.ts
+++ src/out_patients/modules/billing/submodules/deposit/deposit.component.ts
@@ -1,87 +1,82 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { RefundDialogComponent } from "./refund-dialog/refund-dialog.component";
-import { MatDialog } from "@angular/material/dialog";
-import { DepositDialogComponent } from "./deposit-dialog/deposit-dialog.component";
-import { FormGroup } from "@angular/forms";
-import { QuestionControlService } from "@shared/ui/dynamic-forms/service/question-control.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RefundDialogComponent } from './refund-dialog/refund-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { DepositDialogComponent } from './deposit-dialog/deposit-dialog.component';
+import { FormGroup } from '@angular/forms';
+import { QuestionControlService } from '@shared/ui/dynamic-forms/service/question-control.service';
 import { Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: "out-patients-deposit",
-  templateUrl: "./deposit.component.html",
-  styleUrls: ["./deposit.component.scss"],
+  selector: 'out-patients-deposit',
+  templateUrl: './deposit.component.html',
+  styleUrls: ['./deposit.component.scss']
 })
 export class DepositComponent implements OnInit {
-  constructor(
-    public matDialog: MatDialog,
-    private formService: QuestionControlService,
-    private router: Router
-  ) {}
 
+  constructor(public matDialog: MatDialog, private formService:QuestionControlService, private router: Router) { }
+  
   @ViewChild("deposittable") deposittable: any;
 
   depositformdata = {
-    type: "object",
-    title: "",
-    properties: {
-      maxid: {
-        type: "string",
-        // title: "Max ID"
+    type:"object",
+    title:"",
+    properties:{
+      maxid:{
+        type:"string",
+       // title: "Max ID"
       },
-      mobileno: {
-        type: "number",
+      mobileno:{
+        type:"number",
         //title: "Mobile No."
       },
-      checkbox: {
-        type: "checkbox",
-        options: [
-          {
-            title: "",
-          },
-        ],
+      checkbox:{
+        type:"checkbox",
+        options:[{
+          title:""
+        }]
       },
-      dateInput: {
-        type: "date",
-      },
-      totaldeposit: {
-        type: "string",
-        // title:"Total Deposit",
+      dateInput:{
+        type:"date"
+      },     
+      totaldeposit:{
+        type:"string",
+       // title:"Total Deposit",
         defaultValue: "2000.00",
-        readonly: true,
+        readonly:true
       },
-      avalaibledeposit: {
-        type: "string",
-        // title:"Avalaible Deposit",
+      avalaibledeposit:{
+        type:"string",
+       // title:"Avalaible Deposit",
         defaultValue: "1000.00",
-        readonly: true,
+        readonly:true
       },
-      totalrefund: {
-        type: "string",
-        // title:"Total Refund",
+      totalrefund:{
+        type:"string",
+       // title:"Total Refund",
         defaultValue: "0.00",
-        readonly: true,
+        readonly:true
       },
-      remarks: {
-        type: "textarea",
-        //  title:"Remarks",
+      remarks:{
+        type:"textarea",
+      //  title:"Remarks",
         defaultValue: "Write Remarks",
-        readonly: true,
+        readonly:true
       },
       panno: {
-        type: "string",
-      },
+        type: "string"
+      },     
       mainradio: {
         type: "radio",
         required: false,
         options: [
           { title: "Form 60", value: "form60" },
           { title: "Pan card No.", value: "pancardno" },
-        ],
+        ]
       },
-    },
-  };
+    }
+  }
 
   depositconfig: any = {
     clickedRows: true,
@@ -101,7 +96,7 @@ export class DepositComponent implements OnInit {
       "totaltaxvalue",
       "deposithead",
       "servicetype",
-      "operatornameid",
+      "operatornameid",    
     ],
     columnsInfo: {
       deposittype: {
@@ -161,37 +156,34 @@ export class DepositComponent implements OnInit {
     },
   };
 
-  depositForm!: FormGroup;
-  questions: any;
-
+  depositForm !: FormGroup;
+  questions:any;
+  
   private readonly _destroying$ = new Subject<void>();
 
   ngOnInit(): void {
     let formResult = this.formService.createForm(
-      this.depositformdata.properties,
-      {}
+      this.depositformdata.properties,{}
     );
-    this.depositForm = formResult.form;
-    this.questions = formResult.questions;
+    this.depositForm=formResult.form;
+    this.questions=formResult.questions;
   }
-  openrefunddialog() {
-    this.matDialog.open(RefundDialogComponent, {
-      width: "70vw",
-      height: "98vh",
-      data: {
-        Mobile: 9898989898,
-        Mail: "mail@gmail.com",
-      },
-    });
+  openrefunddialog()
+  {
+    this.matDialog.open(RefundDialogComponent, { 
+      width: "70vw", 
+      height: "98vh", 
+      data:{
+        Mobile: 9898989898, 
+        Mail: "mail@gmail.com"
+      }});
   }
-  openDepositdialog() {
-    this.matDialog.open(DepositDialogComponent, {
-      width: "70vw",
-      height: "98vh",
-    });
+  openDepositdialog(){
+    this.matDialog.open(DepositDialogComponent,{width:'70vw',height:'98vh'});
   }
 
-  openinitiatedeposit() {
+  openinitiatedeposit(){
     this.router.navigate(["out-patient-billing", "initiate-deposit"]);
   }
+
 }
