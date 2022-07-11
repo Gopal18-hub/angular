453bc750de0ffe6ee2f5a6e89487c1bb0e742961
300615 <dhanapriya.d@gavstech.com>
2022-07-11 18:29:58 +0530

    Merge branch 'feature/Out_Patients' of https://blrmax-azure.maxhealthcare.com/MAX/HIS_New/_git/HIS-ANGULAR.dhana into feature/Out_Patients

--- src/reports/core/constants/CrystalReport.ts
+++ src/reports/core/constants/CrystalReport.ts
@@ -45,23 +45,102 @@ export namespace CrystalReport {
     return environment.ReportsSampleUrl + `/MIS/OnlinePaymentDetailReport`;
   };
 
+  export const CreateOPVisitSlipReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl + `/CreateOPVisitSlip?${params.VisitNo}`
+    );
+  };
 
+  export const ConsumabaleEntryDetailsReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Billing/OTBilling/ConsumabaleEntryDetails?${params.billno}${params.locationID}${params.MAXID}`
+    );
+  };
+  export const ConsumabaleEntryDetails6inchReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Billing/OTBilling/ConsumabaleEntryDetails?${params.billno}${params.locationID}${params.MAXID}`
+    );
+  };
 
- export const planSummary = (dtpStartDate: Date, dtpEndDate: Date) => {
- return "environment.ReportsSampleurl + /Marketing/PlanSummary?${dtpStartDate}=dtpStartDate${dtpEndDate}=dtpEndDate";
-};
+  export const HappyFamilyPlanAllocationReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Out_Patient/HappyFamilyPlanAllocation?${params.MemberShipNo}${params.FLG}${params.planID}`
+    );
+  };
 
-export const refundreport= (refundBill:String, locationID:number) =>{
+  export const HappyFamilyPlanUtilizationReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Out_Patient/HappyFamilyPlanUtilizationReport?${params.MemberShipNo}`
+    );
+  };
 
-return environment.ReportsSampleurl + "/Billing/refundreport?${refundBill}=refundBill${locationID}=locationID";
-// this.BaseUrl ='http://localhost:55746/MAXHIS/Billing/refundreport.aspx?refundBill='+ this.refundBill + '&locationID=' +this.locationID
+  export const VisitingOTBillingReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Billing/VisitingOTBilling?${params.Opbillid}${params.LocationId}${params.flag}`
+    );
+  };
 
-}
- 
-export const rptrefund= (receiptno:string, locationID:number) =>{
-  return "environment.ReportsSampleurl + /Refund/rpt_refund?${receiptno}=receiptno${locationID}=locationID";
-}
-export const depositreport=(receiptnumber:string, locationID:number) =>{
-  return "environment.ReportsSampleurl + /Deposits/rpt_deposit?${receiptnumber}=receiptnumber${locationID}=locationID";
-}
+  export const PlanLedgerReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Marketing/PlanLedger?${params.MaxID}${params.planid}${params.tempbalance}${params.CmbPlan}`
+    );
+  };
+  export const PlanSummaryReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/Marketing/PlanSummary?${params.FromDate}${params.TodDate}`
+    );
+  };
+  export const DoctorSheduleReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/FrontOfficeReports/DoctorShedule?${params.dtpStartDate}${params.dtpEndDate}${params.datetype}${params.datetype}${params.rd_Doctor}${params.DocID}${params.location}`
+    );
+  };
+  export const DoctorSheduleReportBySp = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/FrontOfficeReports/DoctorShedule?${params.dtpStartDate}${params.dtpEndDate}${params.datetype}${params.datetype}${params.rd_Doctor}${params.Rd_Special}${params.Cmb_Special}${params.specialisationID}`
+    );
+  };
+
+  export const CRPExpiredPatientDetailReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/FrontOfficeReports/CRPExpiredPatientDetail?${params.fromdate}${params.todate}${params.locationid}${params.user}`
+    );
+  };
+
+  export const CROPItemPriceModifiedReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/MIS/CROPItemPriceModified?${params.fromdate}${params.todate}${params.locationid}`
+    );
+  };
+
+  export const OpenScrollReport = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/MIS/OpenScrollReport?${params.cmbopenscrolltype}${params.cmbLocation}${params.fromdate}${params.todate}${params.user}`
+    );
+  };
+
+  export const OpenScrollNanavathi = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/MIS/OpenScrollReport?${params.cmbopenscrolltype}${params.cmbLocation}${params.fromdate}${params.todate}${params.user}`
+    );
+  };
+  export const ServiceTaxReportDataReports = (params: any) => {
+    return (
+      environment.ReportsSampleUrl +
+      `/MIS/ServiceTaxReportData?${params.fromdate}${params.todate}${params.CmbLocation}${params.user}`
+    );
+  };
 }
