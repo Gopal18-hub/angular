af1e2a707af963b24e3ea8ca6380ce6ecd2cc273
Veeru Reddy <mandli.reddy@gavstech.com>
2022-07-12 14:58:17 +0530

    added the sample for the op registration page

--- src/shared/services/report.service.ts
+++ src/shared/services/report.service.ts
@@ -3,6 +3,7 @@ import { environment } from "@environments/environment";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Router } from "@angular/router";
 import "winbox";
+import { DomSanitizer } from "@angular/platform-browser";
 
 declare const WinBox: WinBox.WinBoxConstructor;
 
@@ -10,16 +11,20 @@ declare const WinBox: WinBox.WinBoxConstructor;
   providedIn: "root",
 })
 export class ReportService {
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private san: DomSanitizer
+  ) {}
   baseUrl: string | undefined;
 
   openWindow(reportName: string, reportEntity: string, reportParams: any) {
     const params = "?" + new URLSearchParams(reportParams).toString();
     new WinBox(reportName, {
-      url: `${environment.ReportsApiUrl}${reportEntity}${params}`,
+      url: `${environment.reportTenantUrl}${reportEntity}${params}`,
       x: "center",
       y: "center",
-      width: "910px",
+      width: "930px",
       height: "50%",
     });
   }
