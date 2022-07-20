8dccb1676315e3b4bcaeeceae77723dfdf73ac26
Veeru Reddy <mandli.reddy@gavstech.com>
2022-07-20 18:18:58 +0530

    changes for the billing page

---
+++ src/out_patients/modules/billing/submodules/billing/submodules/services/submodules/consumables/consumables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'out-patients-consumables',
+  templateUrl: './consumables.component.html',
+  styleUrls: ['./consumables.component.scss']
+})
+export class ConsumablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
