dae773d44309da377905078af91023f9b8e71592
Veeru Reddy <mandli.reddy@gavstech.com>
2022-07-20 19:11:22 +0530

    fix for the children route

--- src/reports/src/app/app.module.ts
+++ src/reports/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { CrystalReportModule } from "../../modules/crystal-report";
 import { PromptReportModule } from "../../modules/prompt-report";
@@ -15,6 +16,7 @@ import { MaxHealthMessageDialogModule } from "@shared/ui/message-dialog";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaxHealthMessageDialogModule,
     CrystalReportModule,
     PromptReportModule,
