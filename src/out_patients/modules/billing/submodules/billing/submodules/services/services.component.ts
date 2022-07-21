7fc6287a29b8e263e984daa2f5d0e3245b4ace73
Veeru Reddy <mandli.reddy@gavstech.com>
2022-07-21 16:06:10 +0530

    fixes for the tabs

--- src/out_patients/modules/billing/submodules/billing/submodules/services/services.component.ts
+++ src/out_patients/modules/billing/submodules/billing/submodules/services/services.component.ts
@@ -18,36 +18,45 @@ export class ServicesComponent implements OnInit {
   selectedComponent!: ComponentPortal<any>;
   tabs: any = [
     {
+      id: 1,
       title: "Consultations",
       component: ConsultationsComponent,
     },
     {
+      id: 2,
       title: "Investigations",
       component: InvestigationsComponent,
     },
     {
+      id: 3,
       title: "Health Checkups",
       component: HealthCheckupsComponent,
     },
     {
+      id: 4,
       title: "Procudure & Others",
       component: ProcedureOtherComponent,
     },
     {
+      id: 5,
       title: "Order Set",
       component: OrderSetComponent,
     },
     {
+      id: 6,
       title: "Consumables",
       component: ConsumablesComponent,
     },
   ];
+
+  activeTab: any = this.tabs[0];
 
   constructor() {}
 
   ngOnInit(): void {}
 
   tabChange(tab: any) {
+    this.activeTab = tab;
     this.selectedComponent = new ComponentPortal(tab.component);
   }
 }
