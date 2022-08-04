f9b7ff4d5057124634b85f843eff0940c57405e4
Veeru Reddy <mandli.reddy@gavstech.com>
2022-08-04 14:58:15 +0530

    fixes for payment due

---
+++ src/out_patients/modules/billing/submodules/billing/prompts/consumable-details/consumable-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'out-patients-consumable-details',
+  templateUrl: './consumable-details.component.html',
+  styleUrls: ['./consumable-details.component.scss']
+})
+export class ConsumableDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
