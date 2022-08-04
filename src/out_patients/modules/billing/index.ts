4963a8b9d423a4ef41688db069fc6f5f11ba5d9d
Reginabegum_GAVSTech <regina.abdulla@gavstech.com>
2022-08-04 15:33:27 +0530

    index.ts

--- src/out_patients/modules/billing/index.ts
+++ src/out_patients/modules/billing/index.ts
@@ -67,13 +67,7 @@ import { PostDischargeBillComponent } from "./submodules/post-discharge-follow-u
 import { PostDischargeServicesComponent } from "./submodules/post-discharge-follow-up-billing/submodules/services/post-discharge-services.component";
 import { PostDischargeCreditDetailsComponent } from "./submodules/post-discharge-follow-up-billing/submodules/credit-details/post-discharge-credit-details.component";
 import { PostDischargeConsultationsComponent } from "./submodules/post-discharge-follow-up-billing/submodules/services/submodules/post-discharge-consultations/post-discharge-consultations.component";
-import { PaydueComponent } from "./submodules/billing/prompts/paydue/paydue.component";
-import { InvestigationWarningComponent } from "./submodules/billing/prompts/investigation-warning/investigation-warning.component";
-import { UnbilledInvestigationComponent } from "./submodules/billing/prompts/unbilled-investigation/unbilled-investigation.component";
-import { HealthCheckupWarningComponent } from "./submodules/billing/prompts/health-checkup-warning/health-checkup-warning.component";
-import { ConsumableDetailsComponent } from "./submodules/billing/prompts/consumable-details/consumable-details.component";
-import { ReferalDoctorWarningComponent } from "./submodules/billing/prompts/referal-doctor-warning/referal-doctor-warning.component";
-import { PackageDoctorModificationComponent } from "./submodules/billing/prompts/package-doctor-modification/package-doctor-modification.component";
+import { OprefundDialogComponent } from "./submodules/oprefund-approval/oprefund-dialog/oprefund-dialog.component";
 @NgModule({
   declarations: [
     BillingComponent,
@@ -119,13 +113,7 @@ import { PackageDoctorModificationComponent } from "./submodules/billing/prompts
     PostDischargeServicesComponent,
     PostDischargeCreditDetailsComponent,
     PostDischargeConsultationsComponent,
-    PaydueComponent,
-    InvestigationWarningComponent,
-    UnbilledInvestigationComponent,
-    HealthCheckupWarningComponent,
-    ConsumableDetailsComponent,
-    ReferalDoctorWarningComponent,
-    PackageDoctorModificationComponent,
+    OprefundDialogComponent,
   ],
   imports: [
     BillingRoutingModule,
