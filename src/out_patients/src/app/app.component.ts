54d9648fa15ab310346c10ecce6f070cc7b3af0b
Veeru Reddy <mandli.reddy@gavstech.com>
2022-08-17 15:50:47 +0530

    fix for the out patient routing

--- src/out_patients/src/app/app.component.ts
+++ src/out_patients/src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component, HostListener } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
+import { AuthService } from "@shared/services/auth.service";
 
 @Component({
   selector: "out-patients-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Out Patients";
+
+  isAuthenticated: boolean = false;
 
   @HostListener("window:keydown.Alt.r", ["$event"])
   navigateToRegister($event: any) {
     this.router.navigate(["/registration"]);
   }
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.auth.isAuthenticated();
+  }
 }
