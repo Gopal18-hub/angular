190fb946c65478e2c03fc7645adf7b3c0785a639
Veeru Reddy <mandli.reddy@gavstech.com>
2022-09-03 13:20:01 +0530

    correct the text typo

--- src/out_patients/modules/billing/submodules/billing/billing.component.ts
+++ src/out_patients/modules/billing/submodules/billing/billing.component.ts
@@ -423,6 +423,9 @@ export class BillingComponent implements OnInit {
       const diffYears = today.diff(dobRef, "years");
       const diffMonths = today.diff(dobRef, "months");
       const diffDays = today.diff(dobRef, "days");
+      if (diffMonths == 0 && diffDays == 0) {
+        this.snackbar.open("It’s their birthday today", "info");
+      }
       let returnAge = "";
       if (diffYears > 0) {
         returnAge = diffYears + " Year(s)";
