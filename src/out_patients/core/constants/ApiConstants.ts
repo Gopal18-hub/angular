92ce7b8470671e8b82daf134c5c0571ad616cc50
Reginabegum_GAVSTech <regina.abdulla@gavstech.com>
2022-09-05 18:56:05 +0530

    configure api call

--- src/out_patients/core/constants/ApiConstants.ts
+++ src/out_patients/core/constants/ApiConstants.ts
@@ -1067,16 +1067,39 @@ export namespace ApiConstants {
   };
 
   //Misc Billings
-  export const getcorporatemaster = (flag?: number) => { return (environment.CommonApiUrl + "api/lookup/getcorporatemaster?flag=" + flag) }
+  export const getcorporatemaster = (flag?: number) => {
+    return (
+      environment.CommonApiUrl + "api/lookup/getcorporatemaster?flag=" + flag
+    );
+  };
   export const getinteractionmaster =
     environment.BillingApiUrl + "api/outpatientbilling/getinteractionmaster";
   export const getmasterdataformiscellaneous =
-    environment.BillingApiUrl + "api/outpatientbilling/getmasterdataformiscellaneous";
-  export const getdataforbillreport =
-    (Opbillid: number,
-      Locationid: number,
-      flag: number) => {
-      return (environment.BillingApiUrl + "api/outpatientbilling/getdataforbillreport/" + Opbillid + "/" + Locationid + "/" + flag)
-    }
+    environment.BillingApiUrl +
+    "api/outpatientbilling/getmasterdataformiscellaneous";
+  export const getdataforbillreport = (
+    Opbillid: number,
+    Locationid: number,
+    flag: number
+  ) => {
+    return (
+      environment.BillingApiUrl +
+      "api/outpatientbilling/getdataforbillreport/" +
+      Opbillid +
+      "/" +
+      Locationid +
+      "/" +
+      flag
+    );
+  };
 
+  export const getconfiguremessage = (requesttype: string, value: string) => {
+    return (
+      environment.BillingApiUrl +
+      "api/outpatientbilling/gethl7outboundmessageris/" +
+      requesttype +
+      "/" +
+      value
+    );
+  };
 }
