bd226f22120f67d5779faec803b1069a49413386
Veeru Reddy <mandli.reddy@gavstech.com>
2022-09-30 20:52:07 +0530

    fix for the disable

--- src/out_patients/modules/billing/submodules/billing/submodules/bill/bill.component.ts
+++ src/out_patients/modules/billing/submodules/billing/submodules/bill/bill.component.ts
@@ -108,10 +108,8 @@ export class BillComponent implements OnInit {
         readonly: true,
       },
       coupon: {
-        type: "number",
+        type: "string",
         required: false,
-        defaultValue: 0.0,
-        readonly: true,
       },
       coPay: {
         type: "number",
@@ -162,7 +160,7 @@ export class BillComponent implements OnInit {
         type: "number",
         required: false,
         defaultValue: "0.00",
-        readonly: false,
+        readonly: true,
       },
     },
   };
@@ -645,6 +643,10 @@ export class BillComponent implements OnInit {
                   this.totalDeposit
                 );
                 this.formGroup.controls["dipositAmtEdit"].setValue(0.0);
+                this.formGroup.controls["dipositAmtEdit"].enable();
+                this.question[20].readonly = false;
+                this.question[20].disable = false;
+                this.question[20] = { ...this.question[20] };
                 if (res.data)
                   this.snackbar.open("Deposit Amount availed successfully!");
               });
