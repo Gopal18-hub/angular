273b2083bd2cf5ab97646446b587cd65af5b1ccd
PIYUSHB_EXT <piyushsanjay.b@gavstech.com>
2022-10-10 18:06:51 +0530

    Added auth furard for each module

--- src/reports/modules/prompt-report/routes.ts
+++ src/reports/modules/prompt-report/routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PromptReportComponent } from "./prompt-report.component";
 import { BasicComponent } from "./submodules/basic/basic.component";
+import { AuthGuardService } from "@shared/services/guards/auth-guard.service";
 
 const routes: Routes = [
   {
     path: "prompt-report",
     component: PromptReportComponent,
+    canActivate: [AuthGuardService],
     children: [
       {
         path: ":reportName",
