4535a2522627519f606437255064840c1617f5af
105587 <vimala.muthu@gavstech.com>
2022-10-17 11:31:39 +0530

    miscellaneous bug fix

--- src/out_patients/core/services/calculate-bill.service.ts
+++ src/out_patients/core/services/calculate-bill.service.ts
@@ -254,6 +254,16 @@ export class CalculateBillService {
         formData: {
           authorise: { title: "As Per Policy", value: 4 },
         },
+        discounttypes:
+          [
+            { title: "On Bill", value: "On-Bill" },
+            { title: "On Service", value: "On-Service" },
+            { title: "On Item", value: "On-Item" },
+            { title: "On Patient", value: "On-Patient" },
+            { title: "On Company", value: "On-Company" },
+            { title: "On Campaign", value: "On-Campaign" },
+          ],
+        
       };
     }
     const discountReasonPopup = this.matDialog.open(DisountReasonComponent, {
