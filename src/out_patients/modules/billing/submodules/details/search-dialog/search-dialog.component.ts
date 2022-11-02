3340a4b389224dbb709e139b157ac70647775ece
deena_GAVSTech <deenadhayalan.g@gavstech.com>
2022-11-02 13:30:09 +0530

    Bill Details -FIx

--- src/out_patients/modules/billing/submodules/details/search-dialog/search-dialog.component.ts
+++ src/out_patients/modules/billing/submodules/details/search-dialog/search-dialog.component.ts
@@ -365,7 +365,7 @@ export class SearchDialogComponent implements OnInit {
       else
       {
         this.apiProcessing = false;
-        this.snackbar.open('Invalid Max ID');
+        this.snackbar.open('No Data Available / Invalid Input Data');
       }
     },
     (error) => {
