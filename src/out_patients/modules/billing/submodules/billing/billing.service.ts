bff87ab5cf68b299fb21f0b12e5acb129170bc15
PIYUSHB_EXT <piyushsanjay.b@gavstech.com>
2022-11-12 10:50:48 +0530

    Added condition for BllItems length

--- src/out_patients/modules/billing/submodules/billing/billing.service.ts
+++ src/out_patients/modules/billing/submodules/billing/billing.service.ts
@@ -304,17 +304,19 @@ export class BillingService {
         subItems
       )
       .subscribe((res: any) => {
-        res.forEach((resItem: any, index: number) => {
-          //GAV-1070
-          let quanity = !isNaN(Number(this.billItems[index].qty))
-            ? this.billItems[index].qty
-            : 1;
-          this.billItems[index].price = resItem.returnOutPut;
-          this.billItems[index].totalAmount = quanity * resItem.returnOutPut;
-          this.updateServiceItemPrice(this.billItems[index]);
-        });
-        this.calculateTotalAmount();
-        this.refreshBillTab.next(true);
+        if (this.billItems && this.billItems.length > 0) {
+          res.forEach((resItem: any, index: number) => {
+            //GAV-1070
+            let quanity = !isNaN(Number(this.billItems[index].qty))
+              ? this.billItems[index].qty
+              : 1;
+            this.billItems[index].price = resItem.returnOutPut;
+            this.billItems[index].totalAmount = quanity * resItem.returnOutPut;
+            this.updateServiceItemPrice(this.billItems[index]);
+          });
+          this.calculateTotalAmount();
+          this.refreshBillTab.next(true);
+        }
       });
   }
 
