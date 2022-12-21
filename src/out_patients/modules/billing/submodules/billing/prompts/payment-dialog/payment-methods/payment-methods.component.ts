e135253d3e5f6529098a3d8bc1870235c8ed0fa4
300605 <abirami.murugadoss@gavstech.com>
2022-12-21 19:18:10 +0530

    Gav -1441 Payment screen allows 0.50  Rupees and its editable-fixed

--- src/out_patients/modules/billing/submodules/billing/prompts/payment-dialog/payment-methods/payment-methods.component.ts
+++ src/out_patients/modules/billing/submodules/billing/prompts/payment-dialog/payment-methods/payment-methods.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewInit,
   Component,
   EventEmitter,
   Input,
@@ -55,7 +56,7 @@ export class BillingPaymentMethodsComponent implements OnInit {
   activeTab: any;
 
   bankList: any = [];
-
+  totalamtFlag:boolean=false;
   constructor(
     private formService: QuestionControlService,
     private depositservice: DepositService,
@@ -74,6 +75,8 @@ export class BillingPaymentMethodsComponent implements OnInit {
   async ngOnInit() {
     if (this.config.totalAmount) {
       this.totalAmount = this.config.totalAmount;
+      this.totalamtFlag = (this.totalAmount - Math.floor(this.totalAmount)) !== 0;
+      this.totalAmount= this.totalamtFlag?this.totalAmount:Math.floor(this.totalAmount);
     }
     // //GAV-530 Paid Online appointment
     if (this.config.isonlinepaidappointment) {
@@ -132,6 +135,14 @@ export class BillingPaymentMethodsComponent implements OnInit {
       if (this.paymentForm[method].controls["price"]) {
         this.paymentForm[method].controls["price"].valueChanges.subscribe(
           (res: any) => {
+            if(!this.totalamtFlag){
+              if(String(res)!=String(Math.trunc(res))){
+                this.paymentForm[method].controls["price"].setValue(
+                  Math.trunc(res)
+                )
+              
+              }
+            }
             if (Number(res) < 0) {
               this.messageDialogService.warning("Amount Cannot be Negative");
               this.paymentForm[method].controls["price"].setValue(
@@ -144,7 +155,12 @@ export class BillingPaymentMethodsComponent implements OnInit {
                 (partialSum, a) => partialSum + a,
                 0
               );
-              this.remainingAmount = parseFloat(this.totalAmount) - sum;
+              if(!this.totalamtFlag){
+               this.remainingAmount = parseFloat(this.totalAmount) -Math.floor(sum) ;
+              }
+              else{
+                this.remainingAmount = parseFloat(this.totalAmount) -sum ;
+              }
               if (this.remainingAmount < 0) {
                 this.messageDialogService.warning(
                   "Total of Receipt Amount Cannot be Greater than Bill Amount."
