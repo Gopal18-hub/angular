349723e58a846eb20faef106df86284bd8cd0b82
PIYUSHB_EXT <piyushsanjay.b@gavstech.com>
2023-01-07 14:48:57 +0530

    Chnaged prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,33 +4,15 @@ export const environment = {
   ApiUrl: "",
   cookieUrl: "",
   passwordResetUrl: "https://selfheal.maxhealthcare.com/resetaccount",
-  //dev environment urls
-  CommonApiUrl: "http://172.30.0.16:1009/",
-  PatientApiUrl: "http://172.30.0.16:1008/",
-  IdentityServerUrl: "https://MaxHIS-Idsrv-dev.maxhealthcare.in/",
-  IentityServerRedirectUrl: "http://172.30.0.16/",
-  ReportsApiUrl: "https://MaxHIS-Reports-dev.maxhealthcare.in/",
-  BillingApiUrl: "http://172.30.0.16:1007/",
-  reportTenantUrl: "http://localhost:4205/reports/crystal-report/popup/",
 
-  //local envionment urls for testing
-  // ReportsApiUrl:'https://MaxHIS-Reports-dev.maxhealthcare.in/',
-  // BillingApiUrl:'http://172.30.0.16:1007/',
-  // IdentityServerUrl: 'https://localhost/',
-  // IentityServerRedirectUrl:'http://localhost:8100/',
-  //   CommonApiUrl : 'http://172.30.0.16:1009/',
-  // PatientApiUrl : 'http://172.30.0.16:1008/',
-
-  // CommonApiUrl: "https://MaxHIS-Common-sit.maxhealthcare.in/",
-  // PatientApiUrl: "https://MaxHIS-OpReg-sit.maxhealthcare.in/",
-  // ReportsApiUrl: "https://MaxHIS-Reports-sit.maxhealthcare.in/",
-  // IdentityServerUrl: "https://MaxHIS-Idsrv-sit.maxhealthcare.in/",
-  // IentityServerRedirectUrl: "https://MaxHIS-sit.maxhealthcare.in/",
-
-  //UAT
-  // ReportsApiUrl: "https://MaxHIS-Reports-UAT.maxhealthcare.in/",
-  // IdentityServerUrl: "https://MaxHIS-Idsrv-UAT.maxhealthcare.in/",
-  // IentityServerRedirectUrl: "https://MaxHIS-uat.maxhealthcare.in/",
-  // PatientApiUrl: "https://MaxHIS-OpReg-uat.maxhealthcare.in/",
-  // CommonApiUrl: "https://MaxHIS-Common-uat.maxhealthcare.in/",
+  CommonApiUrl: "https://maxhis.maxhealthcare.in/common",
+  PatientApiUrl: "https://maxhis.maxhealthcare.in/patient",
+  // ReportsApiUrl: "https://maxhis.maxhealthcare.in/reports",
+  IdentityServerUrl: "https://maxhis.maxhealthcare.in/idsrv",
+  IentityServerRedirectUrl: "https://maxhis.maxhealthcare.in/",
+  BillingApiUrl: "https://maxhis.maxhealthcare.in/opbill",
+  reportTenantUrl:
+    "https://maxhis.maxhealthcare.in/reports/crystal-report/popup/",
+  ReportsSampleUrl: "https://maxhis.maxhealthcare.in/reports",
+  // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
 };
