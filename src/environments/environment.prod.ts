b266c1c9b429236aa8a9d414991f6c3742be13f9
PIYUSHB_EXT <piyushsanjay.b@gavstech.com>
2023-01-12 12:59:52 +0530

    Added API Gatway chnages with IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,26 @@ export const environment = {
   ApiUrl: "",
   cookieUrl: "",
   passwordResetUrl: "https://selfheal.maxhealthcare.com/resetaccount",
-  CommonApiUrl: "https://maxhis.maxhealthcare.in/common",
-  PatientApiUrl: "https://maxhis.maxhealthcare.in/patient",
+  //with LB only
+  // CommonApiUrl: "https://maxhis.maxhealthcare.in/common",
+  // PatientApiUrl: "https://maxhis.maxhealthcare.in/patient",
+  // // ReportsApiUrl: "https://maxhis.maxhealthcare.in/reports",
+  // IdentityServerUrl: "https://maxhis.maxhealthcare.in/idsrv",
+  // IentityServerRedirectUrl: "https://maxhis.maxhealthcare.in/",
+  // BillingApiUrl: "https://maxhis.maxhealthcare.in/opbill",
+  // reportTenantUrl:
+  //   "https://maxhis.maxhealthcare.in/reports/crystal-report/popup/",
+  // ReportsSampleUrl: "https://maxhis.maxhealthcare.in/reports",
+  // // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
+
+  //wihtout LB and with API Gatway
+  CommonApiUrl: "https://172.30.0.21/common",
+  PatientApiUrl: "https://172.30.0.21/patient",
   // ReportsApiUrl: "https://maxhis.maxhealthcare.in/reports",
-  IdentityServerUrl: "https://maxhis.maxhealthcare.in/idsrv",
-  IentityServerRedirectUrl: "https://maxhis.maxhealthcare.in/",
-  BillingApiUrl: "https://maxhis.maxhealthcare.in/opbill",
-  reportTenantUrl:
-    "https://maxhis.maxhealthcare.in/reports/crystal-report/popup/",
-  ReportsSampleUrl: "https://maxhis.maxhealthcare.in/reports",
+  IdentityServerUrl: "https://172.30.0.47:1901",
+  IentityServerRedirectUrl: "https://172.30.0.28/",
+  BillingApiUrl: "https://172.30.0.21/opbill",
+  reportTenantUrl: "https://172.30.0.28/reports/crystal-report/popup/",
+  ReportsSampleUrl: "https://172.30.0.21/reports",
   // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
 };
