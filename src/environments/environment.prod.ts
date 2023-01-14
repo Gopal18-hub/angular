87ddb388177f4824607431ffe0ebf4bef067f0bb
PIYUSHB_EXT <piyushsanjay.b@gavstech.com>
2023-01-14 16:00:39 +0530

    latest prod urls with LB and APi gatway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,13 +17,24 @@ export const environment = {
   // // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
 
   //wihtout LB and with API Gatway
-  CommonApiUrl: "https://172.30.0.21/common",
-  PatientApiUrl: "https://172.30.0.21/patient",
-  // ReportsApiUrl: "https://maxhis.maxhealthcare.in/reports",
-  IdentityServerUrl: "https://172.30.0.47:1901",
-  IentityServerRedirectUrl: "https://172.30.0.28/",
-  BillingApiUrl: "https://172.30.0.21/opbill",
-  reportTenantUrl: "https://172.30.0.28/reports/crystal-report/popup/",
-  ReportsSampleUrl: "https://172.30.0.21/reports",
-  // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
+  // CommonApiUrl: "https://172.30.0.21/common",
+  // PatientApiUrl: "https://172.30.0.21/patient",
+  // // ReportsApiUrl: "https://maxhis.maxhealthcare.in/reports",
+  // IdentityServerUrl: "https://172.30.0.47:1901/idsrv",
+  // IentityServerRedirectUrl: "https://172.30.0.28/",
+  // BillingApiUrl: "https://172.30.0.21/opbill",
+  // reportTenantUrl: "https://172.30.0.28/reports/crystal-report/popup/",
+  // ReportsSampleUrl: "https://172.30.0.21/reports",
+  // // crystalReportBaseUrl: "https://maxhis.maxhealthcare.in/reports",
+
+  CommonApiUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/common/",
+  PatientApiUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/patient/",
+  // ReportsApiUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/reports/",
+  IdentityServerUrl: "https://maxhis-idsrv-prod.maxhealthcare.in/",
+  IentityServerRedirectUrl: "https://maxhis.maxhealthcare.in/",
+  BillingApiUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/opbill/",
+  reportTenantUrl:
+    "https://maxhis.maxhealthcare.in/reports/crystal-report/popup/",
+  ReportsSampleUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/reports/",
+  // crystalReportBaseUrl: "https://maxhis-opdapigtwy.maxhealthcare.in/reports/",
 };
